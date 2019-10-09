c814eaee0e2f78822c44c5099bdb0e8c2178b9fa
Suphakit (Ball) Khampan <skh@manaosoftware.com>
2019-10-09 12:48:11 +0700

    Add base service

--- Assignment/Assignment/Services/Customers/ICustomerService.cs
+++ Assignment/Assignment/Services/Customers/ICustomerService.cs
@@ -1,4 +1,5 @@
 ﻿using Assignment.Core.DomainModels.Customers;
+using Assignment.Services.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Assignment.Services.Customers
 {
-    public interface ICustomerService
+    public interface ICustomerService : IService<Customer>
     {
         Task<Customer> GetCustomerById(int customerId);
     }
