a3ab90ec82967cd0a53c00fe04527af2269d51d1
Suphakit (Ball) Khampan <skh@manaosoftware.com>
2019-10-09 13:59:50 +0700

    Add swagger

--- Assignment/Assignment/Startup.cs
+++ Assignment/Assignment/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using StructureMap;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Assignment
 {
@@ -43,6 +44,12 @@ namespace Assignment
                 config.AddRegistry(new StructureMapRegistry());
                 config.Populate(services);
             });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "API Controller", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +67,16 @@ namespace Assignment
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+            });
         }
     }
 }
