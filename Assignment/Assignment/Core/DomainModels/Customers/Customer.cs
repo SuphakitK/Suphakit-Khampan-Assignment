9866c8fe2f76e6d6336f736ecb0d67d10e4eab75
Suphakit (Ball) Khampan <skh@manaosoftware.com>
2019-10-09 10:14:08 +0700

    Add customer model

---
+++ Assignment/Assignment/Core/DomainModels/Customers/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Core.DomainModels.Customers
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
