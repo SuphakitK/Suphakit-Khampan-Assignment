b70052378b09750ff6dc1d62a4471c1e43da3e7c
Suphakit (Ball) Khampan <skh@manaosoftware.com>
2019-10-10 09:24:21 +0700

    Update Transaction model DTO

--- Assignment/Assignment/Core/DomainModels/Transactions/TransactionDTO.cs
+++ Assignment/Assignment/Core/DomainModels/Transactions/TransactionDTO.cs
@@ -8,7 +8,7 @@ namespace Assignment.Core.DomainModels.Transactions
     public class TransactionDTO
     {
         public int Id { get; set; }
-        public DateTimeOffset Date { get; set; }
+        public string Date { get; set; }
         public decimal Amount { get; set; }
         public string Currency { get; set; }
         public StatusTransaction Status { get; set;}
@@ -18,7 +18,7 @@ namespace Assignment.Core.DomainModels.Transactions
             return new TransactionDTO()
             {
                 Id = transaction.Id,
-                Date = transaction.Date,
+                Date = transaction.Date.ToString("dd/MM/yyyy hh:mm"),
                 Amount = transaction.Amount,
                 Currency = transaction.Currency,
                 Status = transaction.Status
