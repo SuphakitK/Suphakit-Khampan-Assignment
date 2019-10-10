ec188339cd6d7a3c9485160bea1e722c06efdea0
Suphakit (Ball) Khampan <skh@manaosoftware.com>
2019-10-10 10:28:41 +0700

    Add New xUnit and some Test case

---
+++ Assignment/Assignment.Test/UnitTests/Validations/CustomerValidationTest.cs
@@ -0,0 +1,21 @@
+﻿using Assignment.Services.Validations;
+using System;
+using Xunit;
+
+namespace Assignment.Test.UnitTests.Validations
+{
+    public class CustomerValidationTest
+    {
+        [Fact]
+        public void GetCustomerById_Should_Success_When_InputPositiveInteger()
+        {
+            //Arrange
+            var customerId = 5;
+            //Act
+            var validationService = new CustomerValidation();
+            var result = validationService.ValidationCustomerId(customerId);
+            //Assert
+            Assert.True(string.IsNullOrEmpty(result));
+        }
+    }
+}
